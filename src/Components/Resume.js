ae888727597237d59708c4e09cfce45281c2b240
Naman Jain <progemer2007128@gmail.com>
2026-04-17 15:01:50 +0530

    updated resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BsDownload } from "react-icons/bs";
-import pdf from "../Resume.pdf";
+import pdf from "./Resume.pdf";
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
